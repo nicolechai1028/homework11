5b26f43c7322bd128dedd2459e597f7659d371d9
Nicole Chai <nicole@namawell.com>
2020-06-16 22:27:32 -0700

    can't seem to create connection

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers_tbl(burger_name) values ('Hamburger');
+insert into burgers_tbl(burger_name) values ('Impossible Burger');
+insert into burgers_tbl(burger_name) values ('Vegan Spicy Burger');
+insert into burgers_tbl(burger_name) values ('Shroom Burger');
